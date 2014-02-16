f451399e9f00d04cfd6f604a968d985192948d11
Julie Kraft <julieschmoo@gmail.com>
2014-02-16 17:58:29 -0500

    template in dinosaur view

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -15,11 +15,11 @@ var DinosaurView = Backbone.View.extend ({
     this.render();
   },
   render: function(){
-    this.$el.html(this.template()(this.model.attributes));
+    this.$el.html(this.template(this.model.attributes));
     console.log(this.model.attributes);
   },
   template: function(attrs){
-    var listTemplate = _.template("My name is <%=this.name%>, and I'm a <%= this.gender %> <%= this.species %>!");
+    var template = _.template("My name is <%=this.name%>, and I'm a <%= this.gender %> <%= this.species %>!");
     return template(attrs);
   },
   el: function(){
